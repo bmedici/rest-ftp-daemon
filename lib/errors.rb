40248f3338a9113671c29206e9056f8094a183bd
Bruno <bmgit@bmconseil.com>
2014-07-31 22:38:37 +0200

    imported config and errors from previous project

---
+++ lib/errors.rb
@@ -0,0 +1,16 @@
+# Error codes
+# O: all ok
+# 1x: request body errors
+# 2x: transfer errors
+
+ERR_OK                            = 0
+ERR_BUSY                          = -1
+
+ERR_REQ_SOURCE_MISSING            = 11
+ERR_REQ_TARGET_MISSING            = 12
+ERR_REQ_TARGET_SCHEME             = 13
+
+ERR_JOB_SOURCE_NOTFOUND            = 21
+ERR_JOB_TARGET_UNPARSEABLE         = 22
+ERR_JOB_PERMISSION                 = 24
+
