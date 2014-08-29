3baea6e90330984d73d54828a5e584d1cf018d68
Bruno <bmgit@bmconseil.com>
2014-08-29 16:11:24 +0200

    updated global config file and exceptions

--- lib/rest-ftp-daemon/config.rb
+++ lib/rest-ftp-daemon/config.rb
@@ -2,30 +2,7 @@ module RestFtpDaemon
   # Global config
   VERSION = "0.22"
 
-  ERRORS = {
-    ERR_JOB_SOURCE_NOTFOUND: 21,
-    ERR_JOB_TARGET_UNPARSEABLE: 22,
-  }
-
   # Transfer config
   TRANSFER_CHUNK_SIZE = 100000
-  THREAD_SLEEP_BEFORE_DIE = 360
-  #THREAD_SLEEP_BEFORE_DIE = 120
-
-  # Errors: global
-  ERR_OK                            = 0
-  ERR_BUSY                          = -1
-
-  # Errors at request level
-  ERR_REQ_SOURCE_MISSING            = 11
-  ERR_REQ_TARGET_MISSING            = 12
-  ERR_REQ_SOURCE_NOTFOUND           = 13
-  ERR_REQ_TARGET_SCHEME             = 15
-
-  # Errors at job level
-  ERR_JOB_SOURCE_NOTFOUND            = 21
-  ERR_JOB_TARGET_UNPARSEABLE         = 22
-  ERR_JOB_PERMISSION                 = 24
-  ERR_JOB_TARGET_PRESENT             = 25
-
+  THREAD_SLEEP_BEFORE_DIE = 30
 end
--- lib/rest-ftp-daemon/exceptions.rb
+++ lib/rest-ftp-daemon/exceptions.rb
@@ -1,12 +1,21 @@
 module RestFtpDaemon
 
-  class RestFtpDaemonException < StandardError
-  end
+  class RestFtpDaemonException < StandardError; end
 
-  class DummyException < RestFtpDaemonException
-  end
+  class DummyException < RestFtpDaemonException; end
 
-  class RequestSourceMissing < RestFtpDaemonException
-  end
+  class RequestSourceMissing     < RestFtpDaemonException; end
+  class RequestSourceNotFound    < RestFtpDaemonException; end
+  class RequestTargetMissing     < RestFtpDaemonException; end
+  class RequestTargetScheme      < RestFtpDaemonException; end
+
+  class JobPrerequisitesNotMet   < RestFtpDaemonException; end
+
+  class JobNotFound              < RestFtpDaemonException; end
+  class JobSourceMissing         < RestFtpDaemonException; end
+  class JobSourceNotFound        < RestFtpDaemonException; end
+  class JobTargetMissing         < RestFtpDaemonException; end
+  class JobTargetUnparseable     < RestFtpDaemonException; end
+  class JobTargetPermission      < RestFtpDaemonException; end
 
 end
