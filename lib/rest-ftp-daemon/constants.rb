765190b85dc2480ebd77465918e4c8ed6cdca528
Bruno <bmgit@bmconseil.com>
2014-11-01 18:26:38 +0100

    moving some constants to constants.rb

---
+++ lib/rest-ftp-daemon/constants.rb
@@ -0,0 +1,10 @@
+# Terrific constants
+APP_NAME = "rest-ftp-daemon"
+APP_CONF = "/etc/#{APP_NAME}.yml"
+APP_VER = "0.81"
+
+# Some defaults
+DEFAULT_CONNECT_TIMEOUT_SEC = 30
+DEFAULT_UPDATE_EVERY_KB = 1024
+DEFAULT_WORKERS = 1
+DEFAULT_LOGS_PROGNAME_TRIM = 9
