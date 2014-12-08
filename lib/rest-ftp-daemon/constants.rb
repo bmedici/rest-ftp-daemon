015fa3f1982e01037ba2749285a68ec2d5f27bcd
Bruno <bmgit@bmconseil.com>
2014-12-08 14:47:16 +0100

    version bump to 0.95

--- lib/rest-ftp-daemon/constants.rb
+++ lib/rest-ftp-daemon/constants.rb
@@ -1,7 +1,7 @@
 # Terrific constants
 APP_NAME = "rest-ftp-daemon"
 APP_CONF = "/etc/#{APP_NAME}.yml"
-APP_VER = "0.94.4"
+APP_VER = "0.95"
 
 # Some global constants
 IDENT_JOB_LEN = 4
@@ -19,5 +19,5 @@ APP_STARTED = Time.now
 APP_LIBS = File.dirname(__FILE__)
 
 # Debugging
-DEBUG_FTP_COMMANDS = false
+DEBUG_FTP_COMMANDS = true
 
