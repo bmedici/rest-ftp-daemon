33fe3c40c96a35c77b8c7bc55597c6f8483bd346
Bruno <bmgit@bmconseil.com>
2015-03-03 00:34:51 +0100

    logger now accepts a variable context hash, and a bunch of lines to dump alongside the message

--- lib/rest-ftp-daemon/logger.rb
+++ lib/rest-ftp-daemon/logger.rb
@@ -2,9 +2,25 @@ class Logger
 
   attr_accessor :pipe
 
-  def info_with_id message, options = {}
-    field_id = "%#{-DEFAULT_LOGS_ID_LEN.to_i}s" % options[:id].to_s
-    add Logger::INFO, "#{field_id} \t#{'  '*(options[:level].to_i+1)}#{message}"
+  def info_with_id message, context = {}
+    # Default context
+    #add Logger::DEBUG,  "info_with_id/context: #{context.inspect} | #{message}"
+    context[:level] ||= 0
+
+    # Common message header
+    field_id = "%#{-DEFAULT_LOGS_ID_LEN.to_i}s" % context[:id].to_s
+    prefix = "#{field_id}\t#{'  '*(context[:level].to_i+1)}"
+
+    # Send main message
+    add Logger::INFO, prefix + message.to_s
+
+    # Dump context lines if provided
+    context[:lines].each do |line|
+      line.strip!
+      next if line.empty?
+      add Logger::INFO, prefix + '   | ' + line[0..DEFAULT_LOGS_TRIM_LINE]
+    end if context[:lines].is_a? Enumerable
+
   end
 
 end
