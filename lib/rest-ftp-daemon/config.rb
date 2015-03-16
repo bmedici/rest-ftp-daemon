5f27002bd9a3da0d99becaef0cb2a2e9ccd1c0d5
Bruno <bmgit@bmconseil.com>
2015-03-16 16:34:30 +0100

    SettingsLogic: adding direct accessor to arbitrary path and dump method

--- lib/rest-ftp-daemon/config.rb
+++ lib/rest-ftp-daemon/config.rb
@@ -17,4 +17,15 @@ class Settings < Settingslogic
     self["pidfile"] || "/tmp/#{APP_NAME}.port#{self['port'].to_s}.pid"
   end
 
+  # Direct access to any depth
+  def at *path
+    #put "Settings.nested: wrong path [#{path.inspect}]" unless path.is_a? Hash
+    path.reduce(Settings) {|m,key| m && m[key.to_s] }
+  end
+
+  # Dump whole settings set to readable YAML
+  def dump
+    self.to_hash.to_yaml( :Indent => 4, :UseHeader => true, :UseVersion => false )
+  end
+
 end
