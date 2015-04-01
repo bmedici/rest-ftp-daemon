81b65375490b6f784ee260e9ec8c930f3aa42b03
Bruno <bmgit@bmconseil.com>
2015-04-01 20:53:28 +0200

    API: merge /routes into debug

--- lib/rest-ftp-daemon/api/debug.rb
+++ lib/rest-ftp-daemon/api/debug.rb
@@ -20,24 +20,11 @@ module RestFtpDaemon
         end
       end
 
-      # get '/memsize' do
-      #   info "GET /memsize"
-      #   ObjectSpace.each_object do |e|
-      #     #puts
-      #     print ObjectSpace.memsize_of(e)
-      #     print "\t"
-      #     print e.class.to_s
-      #     print "\t"
-      #     puts e.inspect[0..80]
-      #     # puts ({
-      #     #   klass: e.class,
-      #     #   size: ObjectSpace.memsize_of(e),
-      #     # }.inspect)
-      #     # puts
-      #   end
-      #   status 200
-      #   []
-      # end
+      get '/routes' do
+        info "GET /routes"
+        status 200
+        return RestFtpDaemon::API::Root::routes
+      end
 
     end
   end
