7c2bbec930ca5e3d5918762eb106e1cbe79633b2
Bruno <bmgit@bmconseil.com>
2015-05-05 18:58:51 +0200

    don’t add message attribute to notification if null

--- lib/rest-ftp-daemon/notification.rb
+++ lib/rest-ftp-daemon/notification.rb
@@ -33,10 +33,10 @@ module RestFtpDaemon
         id:       params[:id].to_s,
         signal:   "#{NOTIFY_PREFIX}.#{params[:event].to_s}",
         error:    params[:error],
-        message:    params[:message],
         host:     Settings.host.to_s,
         }
       body[:status] = params[:status] if params[:status].is_a? Enumerable
+      body[:message] = params[:message].to_s unless params[:message].nil?
       @jid = params[:id]
       log_info "initialized"
 
