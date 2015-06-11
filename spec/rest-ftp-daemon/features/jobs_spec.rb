9dce1faadf3125e109fd4e4edbd6b0897ddf56c5
Dominique Rose-Rosette <drose-rosette@af83.com>
2015-06-11 09:00:50 +0200

    Placeholder for feature test on /jobs

---
+++ spec/rest-ftp-daemon/features/jobs_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe "Jobs", feature: true do
+
+  describe "POST /jobs" do
+
+    context "when params are valid" do
+      it "issues a 201 response"
+    end
+
+  end
+
+end
