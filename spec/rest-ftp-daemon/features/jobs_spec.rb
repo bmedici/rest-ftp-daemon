1f1a55c3112b71d07b9d397d5d3d9b0ad9f5d4df
Dominique Rose-Rosette <drose-rosette@af83.com>
2015-06-17 16:37:01 +0200

    Spec that POSTing to /jobs creates a job

--- spec/rest-ftp-daemon/features/jobs_spec.rb
+++ spec/rest-ftp-daemon/features/jobs_spec.rb
@@ -2,12 +2,44 @@ require "spec_helper"
 
 describe "Jobs", feature: true do
 
-  describe "POST /jobs" do
+  describe "GET /jobs" do
+    let!(:response) { get "/jobs" }
 
-    context "when params are valid" do
-      it "issues a 201 response"
+    it "responds successfully" do
+      expect(response.status).to eq 200
     end
 
-  end
+    it "exposes an array" do
+      expect(JSON.parse(response.body)).to be_an_instance_of(Array)
+    end
+  end # GET /jobs
+
+  describe "POST /jobs" do
+    def jobs_list
+      JSON.parse get("/jobs").body
+    end
+
+    context "when params are valid" do
+      let(:params) do
+        {
+          source: "/tmp/foo",
+          target: "/tmp/bar"
+        }
+      end
+
+      it "issues a 201 response" do
+        expect(
+          post("/jobs", json: params).status
+        ).to eq 201
+      end
+
+      it "creates a new job" do
+        expect {
+          post("/jobs", json: params)
+        }.to change { jobs_list.size }.by(1)
+      end
+    end
+
+  end # POST /jobs
 
 end
