23bd1d29400dba9c98469420a2e034af30f5d245
Bruno <bmgit@bmconseil.com>
2015-07-09 12:02:05 +0200

    refactor Notification, improve output

--- lib/rest-ftp-daemon/notification.rb
+++ lib/rest-ftp-daemon/notification.rb
@@ -12,6 +12,10 @@ module RestFtpDaemon
     attr_accessor :job
 
     def initialize url, params
+      # Remember params
+      @url = url
+      @params = params
+
       # Generate a random key
       @id = Helpers.identifier(NOTIFY_IDENTIFIER_LEN)
       @jid = nil
@@ -19,64 +23,71 @@ module RestFtpDaemon
       # Logger
       @logger = RestFtpDaemon::LoggerPool.instance.get :notify
 
+      # Handle the notification
+      log_info "initialized [#{@url}]"
+      process
+    end
+
+  protected
+
+    def process
       # Check context
-      if url.nil?
-        log_info "skipping (missing url): #{params.inspect}"
+      if @url.nil?
+        log_info "skipping (missing url): #{@params.inspect}"
         return
-      elsif params[:event].nil?
-        log_info "skipping (missing event): #{params.inspect}"
+      elsif @params[:event].nil?
+        log_info "skipping (missing event): #{@params.inspect}"
         return
       end
 
       # Build body and extract job ID if provided
-      body = {
-        id:       params[:id].to_s,
-        signal:   "#{NOTIFY_PREFIX}.#{params[:event]}",
-        error:    params[:error],
+      flags = {
+        id:       @params[:id].to_s,
+        signal:   "#{NOTIFY_PREFIX}.#{@params[:event]}",
+        error:    @params[:error],
         host:     Settings.host.to_s,
         }
-      body[:status] = params[:status] if params[:status].is_a? Enumerable
-      body[:message] = params[:message].to_s unless params[:message].nil?
-      @jid = params[:id]
-      log_info "initialized"
+      flags[:status] = @params[:status] if @params[:status].is_a? Enumerable
+      flags[:message] = @params[:message].to_s unless @params[:message].nil?
+      @jid = @params[:id]
 
-
-      # Send message in a thread
+      # Spawn a dedicated thread
       Thread.new do
-        # Prepare query
-        uri = URI(url)
-        headers = {
-          "Content-Type"  => "application/json",
-          "Accept"        => "application/json",
-          "User-Agent"    => "#{APP_NAME} - #{APP_VER}"
-           }
-        data = body.to_json
-        log_info "sending #{data}"
+        send flags
+      end # end Thread
+    end
 
+    def send flags
+      # Prepare query
+      headers = {
+        "Content-Type"  => "application/json",
+        "Accept"        => "application/json",
+        "User-Agent"    => "#{APP_NAME} - #{APP_VER}"
+         }
+      data = flags.to_json
 
-        # Prepare HTTP client
-        http = Net::HTTP.new uri.host, uri.port
-        # http.initialize_http_header({'User-Agent' => APP_NAME})
+      # Send notification through HTTP
+      uri = URI @url
+      http = Net::HTTP.new uri.host, uri.port
 
-        # Post notification
-        response = http.post uri.path, data, headers
+      # Post notification, handle server response / multi-lines
+      log_info "sending #{data}"
+      response = http.post uri.path, data, headers
+      response_lines = response.body.lines
 
-        # Handle server response / multi-lines
-        response_lines = response.body.lines
-
-        if response_lines.size > 1
-          human_size = Helpers.format_bytes(response.body.bytesize, "B")
-          #human_size = 0
-          log_info "received [#{response.code}] #{human_size} (#{response_lines.size} lines)", response_lines
-        else
-          log_info "received [#{response.code}] #{response.body.strip}"
-        end
-
+      if response_lines.size > 1
+        human_size = Helpers.format_bytes(response.body.bytesize, "B")
+        log_info "received [#{response.code}] #{human_size} (#{response_lines.size} lines)", response_lines
+      else
+        log_info "received [#{response.code}] #{response.body.strip}"
       end
+
+      # Handle exceptions
+      rescue StandardError => ex
+        log_error "EXCEPTION: #{ex.inspect}"
 
     end
 
-  protected
 
     def log_context
       {
