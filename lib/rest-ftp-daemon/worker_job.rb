f5628f0d923f0614cf01109fd11991cfd45f728f
Bruno <bmgit@bmconseil.com>
2015-08-27 13:01:55 +0200

    worker: extract job processing logic

--- lib/rest-ftp-daemon/worker_job.rb
+++ lib/rest-ftp-daemon/worker_job.rb
@@ -16,12 +16,26 @@ module RestFtpDaemon
   protected
 
     def work
-      # Wait for a job to come into the queue
+      # Wait for a job to be available in the queue
       worker_status WORKER_STATUS_WAITING
-      #log_info "waiting"
       job = $queue.pop
 
-      # Prepare the job for processing
+      # Work on this job
+      work_on_job job
+
+      # Clean job and worker statuses
+      job.wid = nil
+      worker_jid nil
+
+      # If job status requires a retry, just restack it
+      #@queue.requeue job
+
+      # Sleep for 1s
+      sleep 1
+    end
+
+    def work_on_job job
+      # Prepare job and worker for processing
       worker_status WORKER_STATUS_RUNNING, "job [#{job.id}]"
       worker_jid job.id
       job.wid = Thread.current.thread_variable_get :wid
@@ -33,8 +47,6 @@ module RestFtpDaemon
 
       # Processing done
       worker_status WORKER_STATUS_FINISHED, "job [#{job.id}]"
-      worker_jid nil
-      job.wid = nil
 
       # Increment total processed jobs count
       $queue.counter_inc :jobs_processed
@@ -42,23 +54,17 @@ module RestFtpDaemon
     rescue RestFtpDaemon::JobTimeout => ex
       log_error "JOB TIMED OUT", ex.backtrace
       worker_status WORKER_STATUS_TIMEOUT
-      worker_jid nil
-      job.wid = nil
 
+      # Inform the job
       job.oops_you_stop_now ex unless job.nil?
-      sleep 1
 
     rescue StandardError => ex
       log_error "JOB UNHDNALED EXCEPTION: #{ex.message}", ex.backtrace
       worker_status WORKER_STATUS_CRASHED
+
+      # Inform the job
       job.oops_after_crash ex unless job.nil?
-      sleep 1
-
-    else
-      # Clean job status
-      job.wid = nil
     end
-
 
     if Settings.newrelic_enabled?
       add_transaction_tracer :work,       category: :task
