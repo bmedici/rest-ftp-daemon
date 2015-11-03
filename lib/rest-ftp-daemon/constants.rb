da0f794d7f116b961995a2ace984c8b2129409c2
Bruno <bmgit@bmconseil.com>
2015-11-03 10:11:08 +0100

    version bump to 0.242.5

--- lib/rest-ftp-daemon/constants.rb
+++ lib/rest-ftp-daemon/constants.rb
@@ -1,7 +1,7 @@
 # Terrific constants
 APP_NAME = "rest-ftp-daemon"
 APP_NICK = "rftpd"
-APP_VER = "0.242.4"
+APP_VER = "0.242.5"
 
 # Provide default config file information
 APP_LIB = File.expand_path File.dirname(__FILE__)
