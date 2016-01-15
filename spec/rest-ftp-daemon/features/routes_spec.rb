9dee6977ff7fd80bec08fc29ec0156c42e522c0d
Bruno <bmgit@bmconseil.com>
2016-01-15 12:53:54 +0100

    remove unnecessary routes test

--- spec/rest-ftp-daemon/features/routes_spec.rb
+++ spec/rest-ftp-daemon/features/routes_spec.rb
@@ -10,11 +10,6 @@ describe "Routes", feature: true do
       expect(response.status).to eq 200
     end
 
-    it "exposes properly formed JSON" do
-      expect { JSON.parse(response.to_s) }.not_to raise_error
-      expect(JSON.parse(response.to_s)).to all(have_key("options"))
-    end
-
   end # GET /
 
 end
