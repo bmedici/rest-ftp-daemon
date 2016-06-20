550e3101bac61a056a2652b772ecadce5a12c39b
Bruno <bmgit@bmconseil.com>
2016-06-20 15:50:00 +0200

    logging: cleanup old local files

--- lib/shared/logger_formatter.rb
+++ lib/shared/logger_formatter.rb
@@ -1,29 +1,31 @@
-class LoggerFormatter
+module Shared
+  class LoggerFormatter
 
-  def self.call severity, datetime, progname, payload
-    # Build common values
-    timestamp = datetime.strftime(LOG_HEADER_TIME)
+    def self.call severity, datetime, progname, payload
+      # Build common values
+      timestamp = datetime.strftime(LOG_HEADER_TIME)
 
-    # Build header
-    header = sprintf LOG_HEADER_FORMAT,
-      timestamp,
-      Process.pid,
-      severity,
-      progname
+      # Build header
+      header = sprintf LOG_HEADER_FORMAT,
+        timestamp,
+        Process.pid,
+        severity,
+        progname
 
-    # If we have a bunch of lines, prefix them and send them together
-    return payload.map do |line|
-      "#{header}#{trimmed(line)}\n"
-    end.join if payload.is_a?(Array)
+      # If we have a bunch of lines, prefix them and send them together
+      return payload.map do |line|
+        "#{header}#{trimmed(line)}\n"
+      end.join if payload.is_a?(Array)
 
-    # Otherwise, just prefix the only line
-    return "#{header}#{trimmed(payload)}\n"
+      # Otherwise, just prefix the only line
+      return "#{header}#{trimmed(payload)}\n"
+    end
+
+  protected
+
+    def self.trimmed line
+      line.to_s.rstrip[0..LOG_MESSAGE_TRIM].force_encoding(Encoding::UTF_8)
+    end
+
   end
-
-protected
-
-  def self.trimmed line
-    line.to_s.rstrip[0..LOG_MESSAGE_TRIM].force_encoding(Encoding::UTF_8)
-  end
-
 end
