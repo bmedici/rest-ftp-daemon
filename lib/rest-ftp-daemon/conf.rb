d31d4b884c81aa14f4523a87b16b6dddf87cf9c7
Bruno <bmgit@bmconseil.com>
2016-06-20 15:02:10 +0200

    conf: skip NewRelic if licence empty, just provide a get_pidfile with no instance var

--- lib/rest-ftp-daemon/conf.rb
+++ lib/rest-ftp-daemon/conf.rb
@@ -25,8 +25,6 @@ class Conf
     attr_reader :spec
     attr_reader :files
     attr_reader :host
-    attr_reader :pidfile
-
   end
 
   def self.init app_root = nil
@@ -66,9 +64,6 @@ class Conf
 
     # Init New Relic
     prepare_newrelic self[:newrelic], self.at(:logs, :newrelic)
-
-    # Prepare PID file is not present
-    prepare_pidfile
 
     # Try to access any key to force parsing of the files
     self[:dummy]
@@ -114,13 +109,13 @@ protected
     @files << File.expand_path(path) if path
   end
 
-  def self.prepare_pidfile
-    self[:pidfile] ||= "/tmp/#{@app_name}-#{@host}-#{self[:port]}.pid"
+  def self.get_pidfile
+    self[:pidfile] || "/tmp/#{@app_name}-#{@host}-#{self[:port]}.pid"
   end
 
   def self.prepare_newrelic section, logfile
     # Disable NewRelic if no config present
-    unless section.is_a?(Hash)
+    unless section.is_a?(Hash) && section[:licence]
       ENV["NEWRELIC_AGENT_ENABLED"] = "false"
       return
     end
