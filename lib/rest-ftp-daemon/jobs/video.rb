b59096b7af54d506099c01368bcf1a0e20f7a76c
Bruno <bmgit@bmconseil.com>
2016-09-01 10:12:14 +0200

    job video: add transformation code

--- lib/rest-ftp-daemon/jobs/video.rb
+++ lib/rest-ftp-daemon/jobs/video.rb
@@ -14,10 +14,26 @@ module RestFtpDaemon
     end
 
     def work
+      # Guess source files from disk
+      set_status JOB_STATUS_TRANSFORMING
+      sources = scan_local_paths @source_loc.path
+      raise RestFtpDaemon::SourceNotFound if sources.empty?
 
+      # Add the source file name if none found in the target path
+      target_final = @target_loc.clone
+      target_final.name = @source_loc.name unless target_final.name
+      log_info "JobVideo.work target_final.path [#{target_final.path}]"
 
+      # Ensure target directory exists
+      log_info "JobVideo.work mkdir_p [#{@target_loc.dir}]"
+      FileUtils.mkdir_p @target_loc.dir
 
+      # Do the work, for each file
+      set_info :source, :current, @source_loc.name
+      video_command @source_loc, target_final
 
+      # Done
+      set_info :source, :current, nil
 
     rescue FFMPEG::Error => exception
       return oops :ended, exception, "ffmpeg_error"
@@ -28,36 +44,33 @@ module RestFtpDaemon
       set_info :source, :current, nil
     end
 
+  protected
 
     def video_command source, target
-      log_info "JobVideo.video_command [#{source.name}]: [#{source.full}] > [#{target.full}]"
+      log_info "JobVideo.video_command [#{source.name}]: [#{source.path}] > [#{target.path}]"
       set_info :source, :current, source.name
 
       # Read info about source file
-      movie = FFMPEG::Movie.new(source.full)
-      #set_info :source_video, :total, @transfer_total
+      movie = FFMPEG::Movie.new(source.path)
 
       # Build options
-      ffmpeg_cutsom_options = {
+      ffmpeg_custom_options = {
         audio_codec: @video_ac,
         video_codec: @video_vc,
         custom: ffmpeg_custom_option_array,
         }
-      set_info :video, :ffmpeg_cutsom_options, ffmpeg_cutsom_options
+      set_info :work, :ffmpeg_custom_options, ffmpeg_custom_options
 
       # Build command
-      movie.transcode(target.full, ffmpeg_cutsom_options) do |ffmpeg_progress|
         set_info :video, :ffmpeg_progress, ffmpeg_progress
+      movie.transcode(target.path, ffmpeg_custom_options) do |ffmpeg_progress|
 
         percent0 = (100.0 * ffmpeg_progress).round(0)
         set_info :transfer, :progress, percent0
 
         log_debug "progress #{ffmpeg_progress}"
       end
-
     end
-
-  private
 
     def ffmpeg_custom_option_array
       # Ensure options ar in the correct format
