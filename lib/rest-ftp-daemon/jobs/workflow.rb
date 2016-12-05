4680e878b82b5a1ea07708934aa418340ea4d77a
Bruno Medici <bm.git@bmconseil.com>
2016-12-05 10:58:04 +0100

    workflow: run tasks chain

--- lib/rest-ftp-daemon/jobs/workflow.rb
+++ lib/rest-ftp-daemon/jobs/workflow.rb
@@ -3,5 +3,50 @@
 module RestFtpDaemon
   class JobWorkflow < Job
 
+  protected
+
+    def do_before
+      # Init
+      pipe = {
+        import: TaskImport,
+        transf: TaskTransform,
+        export: TaskExport,
+      }
+      @tasks = {}
+
+      # First input is provided
+      (in1 = @source_loc.clone).name = "in1"
+      (in2 = @source_loc.clone).name = "in2"
+      (in3 = @source_loc.clone).name = "in3"
+      current = [in1, in2, in3]
+      log_debug "WORKFLOW INPUTS", current.map(&:path)
+
+      # Chain every task in the pipe
+      pipe.each do |name, family|
+
+        # Instantiate and store task
+        task = family.new(name, self.id, self.wid)
+        @tasks[name] = task
+
+        # Plug input to previous output
+        task.inputs = current
+
+        # Remember pointer to this output
+        current = task.outputs
+      end
+    end
+
+    def do_work
+      # Run tasks
+      @tasks.each do |n, t|
+        t.do_before
+        t.work
+        t.do_after
+       end
+    end
+
+    def do_after
+    end
+
   end
 end
