5131108f4146da2f2a02a7e4e21456639ddd6e81
Bruno Medici <bm.git@bmconseil.com>
2016-12-05 18:29:02 +0100

    task export: add checks to do_before

--- lib/rest-ftp-daemon/tasks/export.rb
+++ lib/rest-ftp-daemon/tasks/export.rb
@@ -1,5 +1,15 @@
 module RestFtpDaemon
   class TaskExport < Task
+    def do_before
+      super
+
+      # Check source
+      raise RestFtpDaemon::TargetUnsupported, "accepts only one target" if @outputs.size>1
+
+      # Guess target file name, and fail if present while we matched multiple sources
+      @output = @outputs.first
+      raise RestFtpDaemon::TargetDirectoryError, "target should be a directory when matching many files" if @output.name && @inputs.count>1
+    end
 
     def work
       work_debug
