8c4ada0c60a1284c10e7e840c0e45cdbf0ac313c
Bruno Medici <bm.git@bmconseil.com>
2016-12-05 11:02:39 +0100

    task: before/work/after methods

--- lib/rest-ftp-daemon/task.rb
+++ lib/rest-ftp-daemon/task.rb
@@ -1,14 +1,33 @@
 module RestFtpDaemon
   class Task
-    # include Singleton
     include BmcDaemonLib::LoggerHelper
+
+    # Class options
+    attr_reader :jobs
+    attr_reader :name
+    attr_accessor :inputs
+    attr_accessor :outputs
+
     def initialize name, jid = nil, wid = nil
+      # Init context
+      @inputs = []
+      @outputs = []
       @name = name
       @jid = jid
       @wid = wid
 
-      # Logger # FIXME: should be :jobs
       log_pipe      :workflow
+    end
+
+    def do_before
+      log_debug "inputs", @inputs.map(&:path)
+    end
+
+    def work
+    end
+
+    def do_after
+      log_debug "outputs", @outputs.map(&:path)
     end
 
   private
