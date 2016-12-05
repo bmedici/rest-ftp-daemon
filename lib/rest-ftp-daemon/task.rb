a9a95ee6e87edf1f48bc68f953c7172e2ee9e821
Bruno Medici <bm.git@bmconseil.com>
2016-12-05 11:02:03 +0100

    task: add logging stuff

--- lib/rest-ftp-daemon/task.rb
+++ lib/rest-ftp-daemon/task.rb
@@ -1,5 +1,25 @@
 module RestFtpDaemon
   class Task
-  
+    # include Singleton
+    include BmcDaemonLib::LoggerHelper
+    def initialize name, jid = nil, wid = nil
+      @name = name
+      @jid = jid
+      @wid = wid
+
+      # Logger # FIXME: should be :jobs
+      log_pipe      :workflow
+    end
+
+  private
+
+    def log_context
+      {
+        wid: @wid,
+        jid: @jid,
+        id: name
+      }
+    end
+
   end
 end
