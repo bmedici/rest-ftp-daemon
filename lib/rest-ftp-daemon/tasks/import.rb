0f83d5b2e97af82f2d3a12f5e4b28dbcc125f99a
Bruno Medici <bm.git@bmconseil.com>
2016-12-05 18:29:13 +0100

    task import: add checks to do_before

--- lib/rest-ftp-daemon/tasks/import.rb
+++ lib/rest-ftp-daemon/tasks/import.rb
@@ -1,5 +1,14 @@
 module RestFtpDaemon
   class TaskImport < Task
+    def do_before
+      super
+
+      # Check source
+      raise RestFtpDaemon::SourceUnsupported, "accepts only one source" if @inputs.size >1
+
+      @input = @inputs.first
+      raise RestFtpDaemon::SourceUnsupported, @input.scheme unless @input.is? URI::FILE
+    end
 
     def work
       work_debug
