e7ce6731a14b4dcba2576e97c6b9da29aa8ccdda
Bruno Medici <bm.git@bmconseil.com>
2017-06-27 17:38:02 +0200

    task import: update

--- lib/rest-ftp-daemon/tasks/task_import.rb
+++ lib/rest-ftp-daemon/tasks/task_import.rb
@@ -7,17 +7,15 @@ module RestFtpDaemon
     end
 
     # Task operations
-    def prepare
-      # Check input conformity
-      unless @job.source_loc.is_a?(Location) && @job.source_loc.uri_is?(URI::FILE)
-        raise RestFtpDaemon::SourceUnsupported, @job.source_loc.scheme
-      end
-    end
-
     def process
-      # Scan local source files from disk
       set_status Job::STATUS_IMPORT_LISTING
-      # Sump some informations
+
+      # Check input conformity
+      unless source_loc.is_a?(Location) && source_loc.uri_is?(URI::FILE)
+        raise RestFtpDaemon::SourceUnsupported, source_loc.scheme
+      end
+
+      # Scan local source files from disk
       files = source_loc.local_files
       set_info INFO_SOURCE_COUNT, files.size
       set_info INFO_SOURCE_FILES, files.collect(&:name)
@@ -27,14 +25,15 @@ module RestFtpDaemon
 
       # Add file to output
       files.each do |file|
+        log_info "matched: #{file.path_rel}"
         add_output file
       end
     end
 
-    def finalize
-    end
-
   protected
+
+    def process_unit unit
+    end
 
   end
 end
